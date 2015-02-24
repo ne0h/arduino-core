e2344795e58559d25360596897cea9eb6efa2b2e
Max <hess@blacklight-it.com>
2015-02-24 19:12:51 +0100

    Extended the example to show some info via serial rs232

--- test_blink/main.cpp
+++ test_blink/main.cpp
@@ -2,11 +2,14 @@
 
 void setup() {
 	pinMode(13, OUTPUT);
+	Serial.begin(9600);
 }
 
 void loop() {
 	  digitalWrite(13, HIGH);
+	  Serial.write("High");
 	  delay(1000);
 	  digitalWrite(13, LOW);
+	  Serial.write("Low");
 	  delay(1000);
 }
